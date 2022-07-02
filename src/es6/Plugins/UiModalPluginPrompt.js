de1173253c6d736439d810aa8c8aecce21405a4c
dasiux <me@siux.info>
2022-07-02 12:25:22 +0200

    prompt mode

---
+++ src/es6/Plugins/UiModalPluginPrompt.js
@@ -0,0 +1,135 @@
+/**
+ * Requires
+ */
+import { UiPlugin } from '@squirrel-forge/ui-core';
+import { Exception, bindNodeList } from '@squirrel-forge/ui-util';
+
+
+/**
+ * Ui modal plugin prompt exception
+ * @class
+ * @extends Exception
+ */
+class UiModalPluginPromptException extends Exception {}
+
+/**
+ * Ui modal plugin prompt mode
+ * @class
+ * @extends UiPlugin
+ */
+export class UiModalPluginPrompt extends UiPlugin {
+
+    /**
+     * Plugin name getter
+     * @public
+     * @static
+     * @return {string} - Plugin name
+     */
+    static get pluginName() {
+        return 'prompt';
+    }
+
+    /**
+     * Constructor
+     * @constructor
+     * @param {null|Object} options - Options object
+     * @param {Object|UiModalComponent} context - Plugin context
+     * @param {null|console|Object} debug - Debug object
+     */
+    constructor( options, context, debug ) {
+        super( options, context, debug );
+
+        // Extend default config
+        this.extendConfig = {
+
+            // Available modes
+            // @type {Array<mode>}
+            availableModes : [ 'modal', 'prompt' ],
+
+            // Focus first contained element when shown
+            // @type {boolean|Array<config.mode>}
+            focusOnShown : [ 'all!', '!prompt' ],
+
+            // Dom references
+            // @type {Object}
+            dom : {
+
+                // Plugin references
+                prompt : {
+
+                    // Input element reference
+                    // @type {string}
+                    input : '[name="value"]',
+
+                    // Cancel buttons
+                    // @type {string}
+                    cancel : '[data-modal="ctrl:prompt.cancel"]',
+
+                    // Confirm buttons
+                    // @type {string}
+                    confirm : '[data-modal="ctrl:prompt.confirm"]',
+                }
+            }
+        };
+
+        // Register events
+        this.registerEvents = [
+            [ 'initialized', ( event ) => { this.#event_initialized( event ); } ],
+            [ 'modal.shown', () => { this.#focus_on_shown(); } ],
+            [ 'modal.hide', ( event ) => {
+                if ( !this.context.dispatchEvent( 'modal.prompt.cancel', null, true, true ) ) {
+                    event.preventDefault();
+                }
+            } ],
+        ];
+    }
+
+    /**
+     * Event initialized
+     * @private
+     * @param {Event} event - Initialized event
+     * @return {void}
+     */
+    #event_initialized( event ) {
+        if ( event.detail.target !== this.context ) return;
+
+        // Confirm buttons
+        bindNodeList( this.context.getDomRefs( 'prompt.confirm' ), [
+            [ 'click', ( event ) => {
+                event.preventDefault();
+                if ( this.context.dispatchEvent( 'modal.prompt.confirm', { value : this.#get_input_value() }, true, true ) ) {
+                    this.context.hide();
+                }
+            } ]
+        ] );
+    }
+
+    /**
+     * Focus prompt input after modal.shown event
+     * @private
+     * @return {void}
+     */
+    #focus_on_shown() {
+        if ( this.mode === 'prompt' ) {
+            const element = this.getDomRefs( 'prompt.input', false );
+            if ( element ) element.focus();
+        }
+    }
+
+    /**
+     * Get input value
+     * @private
+     * @return {null|boolean}
+     */
+    #get_input_value() {
+        const input = this.context.getDomRefs( 'prompt.input', false );
+        if ( !input || ![ 'input', 'select', 'textarea' ].includes( input.tagName.toLowerCase() ) ) {
+            throw new UiModalPluginPromptException( 'Missing or invalid prompt input element' );
+        }
+        if ( input.type === 'checkbox' || input.type === 'radio' ) {
+            if ( input.checked ) return input.value || true;
+            return false;
+        }
+        return input.value || null;
+    }
+}
