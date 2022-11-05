735d8905c9c742431e2d89a6ff48cc956f6be262
dasiux <me@siux.info>
2022-11-05 03:27:09 +0100

    update version

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 /* !
  * @module      : @squirrel-forge/ui-modal
- * @version     : 0.6.0
+ * @version     : 0.6.3
  * @license     : MIT
  * @copyright   : 2022 squirrel-forge
  * @author      : Daniel Hartwell aka. siux <me@siux.info>
